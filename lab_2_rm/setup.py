53fb5fe933e207d71206e9640879e4114a5e191d
Paulo Oses <paulo.oses@uc.cl>
2025-05-25 21:40:29 -0400

    se agrela el launch de la parte 2 del lab

--- lab_2_rm/setup.py
+++ lab_2_rm/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/lab_2_rm/launch', ['launch/navegacion_pasillo.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
